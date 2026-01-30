28923440b9b54019c9bd659da26ca08faa513a20
Kyle Morck <kyle.morck@gmail.com>
2026-01-29 23:54:42 -0700

    feat: enable HTTP request logging by default

--- src/main.rs
+++ src/main.rs
@@ -240,7 +240,11 @@ fn create_default_user_prompt(
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env().add_directive("cutman=info".parse()?))
+        .with_env_filter(
+            EnvFilter::from_default_env()
+                .add_directive("cutman=info".parse()?)
+                .add_directive("tower_http=debug".parse()?),
+        )
         .init();
 
     let cli = Cli::parse();
